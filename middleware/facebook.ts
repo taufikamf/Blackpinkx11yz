371f7ec1c621f7863a16ad8669de4c88787fcf42
taufikamf <taufikabdul354@gmail.com>
2023-05-04 22:05:41 +0700

    query

--- middleware/facebook.ts
+++ middleware/facebook.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware( async (to, from) => {
   const url = to.params
   const fbclid = from.query.fbclid
-  const fullpath = from.fullPath
+  const fullpath = from.query
   if(fbclid){
     if(url.slug){
       const res = await fetch(`https://thedramaclubs.com/wp-json/wp/v2/posts?slug=${url.slug}`)
@@ -14,7 +14,7 @@ export default defineNuxtRouteMiddleware( async (to, from) => {
         external: true
       })
     }
-  }else if(fullpath.includes("php")){
+  }else if(fullpath){
     if(url.slug){
       const res = await fetch(`https://thedramaclubs.com/wp-json/wp/v2/posts?slug=${url.slug}`)
       const data = await res.json()
