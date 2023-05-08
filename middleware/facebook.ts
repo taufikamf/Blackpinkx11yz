3e5f6ecd6e3a155f1e0e98f09139933a441d8515
taufikamf <taufikabdul354@gmail.com>
2023-05-08 22:45:28 +0700

    fixing redirect fb

--- middleware/facebook.ts
+++ middleware/facebook.ts
@@ -1,34 +1,45 @@
-export default defineNuxtRouteMiddleware( async (to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log(from)
   const runtimeConfig = useRuntimeConfig()
   const url = to.params
   const fbclid = from.query.fbclid
-  const query = from.query
-  if(fbclid){
-    if(url.slug){
+  if (fbclid) {
+    if (url.slug) {
       const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
       const data = await res.json()
       navigateTo(data[0].link, {
         external: true
       })
-    }else{
+    } else {
       navigateTo(`${runtimeConfig.public.API_URL}`, {
         external: true
       })
     }
-  }else if(from.query.hasOwnProperty('fb')){
-    console.log('query')
-    if(url.slug){
+  } else if (from.query.hasOwnProperty('fb')) {
+    if (url.slug) {
       const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
       const data = await res.json()
       navigateTo(data[0].link, {
         external: true
       })
-    }else{
+    } else {
       navigateTo(`${runtimeConfig.public.API_URL}`, {
         external: true
       })
     }
-  }else{
+  } else if (from.path != '/') {
+    if (url.slug) {
+      const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
+      const data = await res.json()
+      navigateTo(data[0].link, {
+        external: true
+      })
+    } else {
+      navigateTo(`${runtimeConfig.public.API_URL}`, {
+        external: true
+      })
+    }
+  } else {
     navigateTo(to.fullPath)
   }
 });
